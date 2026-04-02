3a39dca50b77eaea2f87455e068395f707ccb405
kazu <129391973+kz7c@users.noreply.github.com>
2026-04-02 22:19:11 +0900

    add vertical.cpp

---
+++ vertical.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+int main(){
+    while (true){
+        cout << "#      " << endl;
+        this_thread::sleep_for(chrono::milliseconds(20));
+        cout << " #     " << endl;
+        this_thread::sleep_for(chrono::milliseconds(20));
+        cout << "  #    " << endl;
+        this_thread::sleep_for(chrono::milliseconds(20));
+        cout << "   #   " << endl;
+        this_thread::sleep_for(chrono::milliseconds(20));
+        cout << "    #  " << endl;
+        this_thread::sleep_for(chrono::milliseconds(20));
+        cout << "     # " << endl;
+        this_thread::sleep_for(chrono::milliseconds(20));
+        cout << "      #" << endl;
+        this_thread::sleep_for(chrono::milliseconds(20));
+        cout << "     # " << endl;
+        this_thread::sleep_for(chrono::milliseconds(20));
+        cout << "    #  " << endl;
+        this_thread::sleep_for(chrono::milliseconds(20));
+        cout << "   #   " << endl;
+        this_thread::sleep_for(chrono::milliseconds(20));
+        cout << "  #    " << endl;
+        this_thread::sleep_for(chrono::milliseconds(20));
+        cout << " #     " << endl;
+        this_thread::sleep_for(chrono::milliseconds(20));
+        cout << "#      " << endl;
+        this_thread::sleep_for(chrono::milliseconds(20));
+    }
+
+}
